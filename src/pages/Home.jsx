7e76d1d5eedad750eaa17c6abe190d38a7bf9bf8
Kritika Chahar <kcundercover4eva@gmail.com>
2025-12-27 16:24:08 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -46,7 +46,7 @@ export default function Home() {
       '--accent-primary': theme.primary,
       '--accent-secondary': theme.secondary
     }}>
-      <main className="flex-1 overflow-y-auto overflow-x-hidden pb-24 pt-2">
+      <main className="flex-1 overflow-y-auto overflow-x-hidden pb-24 pt-6">
         <div>
           {appTab === 'dashboard' && <Dashboard />}
           {appTab === 'quiz' && <StudyHub />}
